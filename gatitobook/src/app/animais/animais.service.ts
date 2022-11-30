7c3ac1cb6bbb8fa98f40685ace0cc0fa074c07a9
marcos.lima <marcos.lima@scaleup.com.br>
2022-11-30 16:50:08 -0300

    first commit

---
+++ gatitobook/src/app/animais/animais.service.ts
@@ -0,0 +1,22 @@
+import { Animais } from './animais';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { TokenService } from '../autenticacao/token.service';
+
+const API = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimaisService {
+  constructor(private http: HttpClient, private tokenSevice: TokenService) {}
+
+  listDoUsuario(nomeDoUsuario: string): Observable<Animais> {
+    const token = this.tokenSevice.retornaToken();
+    const headers = new HttpHeaders().append('x - access - token', token);
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
+      headers,
+    });
+  }
+}
