df504095048a62b78c69367bb8bec197c4f451ce
marcos.lima <marcos.lima@scaleup.com.br>
2022-12-01 11:17:15 -0300

    Change in var API

--- gatitobook/src/app/animais/animais.service.ts
+++ gatitobook/src/app/animais/animais.service.ts
@@ -1,10 +1,11 @@
+import { environment } from './../../environments/environment';
 import { Animais } from './animais';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TokenService } from '../autenticacao/token.service';
 
-const API = 'http://localhost:3000';
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root',
--- gatitobook/src/app/animais/animal/animal.component.ts
+++ gatitobook/src/app/animais/animal/animal.component.ts
@@ -1,6 +1,7 @@
+import { environment } from 'src/environments/environment';
 import { Component, OnInit, Input } from '@angular/core';
 
-const API = 'http://localhost:3000';
+const API = environment.apiURL;
 
 @Component({
   selector: 'app-animal',
